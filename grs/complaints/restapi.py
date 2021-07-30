11808ae803ba2b791529c053f3fdf79c0757b3e3
Krunalkumar Shah <krunalkumar@mountblue.io>
2021-07-30 11:47:11 +0530

    permissions added

--- grs/complaints/restapi.py
+++ grs/complaints/restapi.py
@@ -1,18 +1,23 @@
-from rest_framework import viewsets
-from .serializer import ComplaintSerializer
-from .models import Complaint
-from rest_framework.authentication import (TokenAuthentication,
-                                           BasicAuthentication,
-                                           SessionAuthentication)
-from rest_framework.permissions import IsAuthenticated
-# AllowAny can be used
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class ComplaintViewSet(viewsets.ModelViewSet):
-    serializer_class = ComplaintSerializer
-    queryset = Complaint.objects.all()
-    authentication_classes = (BasicAuthentication,
-                              TokenAuthentication,
-                              SessionAuthentication,
-                              )
-    permission_classes = (IsAuthenticated, )
+class ComplaintIsOwnerOrReadOnly(BasePermission):
+    message = 'You have not filed this complaint!'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        elif request.user.is_authenticated:
+            return obj.user_id == request.user or request.user.is_admin
+        return False
+
+
+class UserIsOwnerOrReadOnly(BasePermission):
+    message = 'You have not filed this complaint!'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        elif request.user.is_authenticated:
+            return obj.id == request.user.id or request.user.is_admin
+        return False
