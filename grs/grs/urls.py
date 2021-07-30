d24a76942ffa231dcf89b8442e60c0726d505e3f
Krunalkumar Shah <krunalkumar@mountblue.io>
2021-07-30 11:37:42 +0530

    urls simplified

--- grs/grs/urls.py
+++ grs/grs/urls.py
@@ -15,15 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from complaints import restapi
-from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken import views
-
-router = DefaultRouter()
-router.register(r'Complaint', restapi.ComplaintViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
+    path('', include('complaints.urls')),
+    path('rest-auth/', include('rest_auth.urls'))
 ]
